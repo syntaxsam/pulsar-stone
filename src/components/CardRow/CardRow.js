c3f0a40eb6bdeaf7abcccf70b031646e3eaa8ce3
Samuel Smith <sam.smith@musicbed.com>
2017-12-29 01:52:10 -0600

    add mtg api

--- src/components/CardRow/CardRow.js
+++ src/components/CardRow/CardRow.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import autobind from "react-autobind";
 import PropTypes from "prop-types";
+import autobind from "react-autobind";
 import { StyledCardRow, CardName, Details, ExpandButton, ExpandedRow, ExpandedRowDetails } from './styles/StyledCardRow'
 import AttuneWithAether from './images/attune-with-aether_thumb.png';
 
@@ -18,38 +18,35 @@ class CardRow extends Component {
     this.setState({ isExpanded: !this.state.isExpanded })
   }
 
-  // Practice multiple states functions
-  // withComponent Method Glamorous
-  // have a component that passes props to a child component..
-  // have a button to collaps or expand all rows.. 
-  // Close row when click on other row..
-  // componentWillRecieveProps will recieve props in react docs
-  // if you want update child components state from outside you have to listen for it gotta do a lil bit of magic..
-  // react matches
-
   render() {
     const { isExpanded } = this.state;
-
     return (
       <StyledCardRow
         isExpanded={isExpanded}
         onClick={this.handleClick}
       >
         <Details>
-          <img src={AttuneWithAether} alt="AttuneWithAether" />
-          <CardName>Attune with Aether</CardName>
+          <CardName>{this.props.name}</CardName>
         </Details>
         <ExpandButton className="expandButton">
           {!isExpanded ? 'Expand' : 'Expanded'}
         </ExpandButton>
         {isExpanded && (
           <ExpandedRow isExpanded={isExpanded}>
-            <ExpandedRowDetails>Expanded Row Parts</ExpandedRowDetails>
+            <ExpandedRowDetails>
+              <img src={this.props.imageUrl} alt={this.props.name} />
+            </ExpandedRowDetails>
           </ExpandedRow>
         )}
       </StyledCardRow>
     );
   }
+}
+
+CardRow.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  imageUrl: PropTypes.string
 }
 
 export default CardRow
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from 'axios';
+import autobind from "react-autobind";
+import PropTypes from "prop-types";
 import Sidebar from '../components/Sidebar/Sidebar'
 import CardRow from '../components/CardRow/CardRow'
-import { StyledLayout, Content, Text } from '../components/Layout/styles/StyledLayout'
+import { StyledLayout, Content } from '../components/Layout/styles/StyledLayout'
 
-const Home = () =>
-  <StyledLayout>
-    <Sidebar />
-    <Content>
-      <CardRow />
-      <CardRow />
-      <Text css={{ marginTop: '30px' }}>
-        Over the past 800 years, Agmar, a powerful sorcerer, has gained control
-        of Jardim through violence and intimidation. Agmar’s motivation is power
-        and he gains power by feeding off the suffering of others. His magic is
-        darker than anyone can fathom, the limits to his power are unknown, even to
-        himself. Under Agmar’s command are the men and women known as the Zarlots.
-        The Zarlots surrendered their souls to Agmar to escape death and now serve
-        him. He uses the Zarlots as assassins, spies, and anything else that will
-        further his progression.
-      </Text>
-      <Text>
-        Agmar must leave Jardim to find another world to conquer. He will not give
-        up his power and die with the rest of the world. Using the pulsar stone, Agmar
-        creates portals to send his minions off-world to search for a suitable
-        replacement world. After a long time one of his minions finds Manirem.
-      </Text>
-    </Content>
-  </StyledLayout>
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    autobind(this);
+    this.state = {
+      cards: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get("https://api.magicthegathering.io/v1/cards")
+      .then((response) => {
+        this.setState({
+          cards: response.data.cards
+        })
+      })
+  }
+
+  render() {
+    return (
+      <StyledLayout>
+        <Sidebar />
+        <Content>
+          {
+            this.state.cards.map((card) =>
+              <CardRow key={card.id} name={card.name} imageUrl={card.imageUrl} />
+            )
+          }
+        </Content>
+      </StyledLayout>
+    )
+  }
+}
 
 export default Home
